febf76603cde5a237c26a5a6167ce80d557b0327
Abhiraj Kumar Singh <abhiraj.kumar2300@gmail.com>
2025-01-21 07:59:11 +0530

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# backend/app/models/__init__.py
-from .cve import CVE
-
-__all__ = ["CVE"]
--- app/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-# backend/app/routes/__init__.py
-
-from .cve import router as cve_router
-
-__all__ = ["cve_router"]
